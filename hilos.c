83f8ab63069aa7b8cde273c06dafb5861be08779
atellezm1 <atellezm@unjbg.edu.pe>
2026-09-23 10:18:35 -0500

    se implemento la sincronizacion para la ejecucion de los hilos

--- hilos.c
+++ hilos.c
@@ -5,14 +5,26 @@ void* hilo_funcion (void* arg) {
 	printf("Hola desde el hilo %ld\n", (long) arg); 
 	pthread_exit(NULL);
 }
+
+pthread_mutex_t mutex;
+
+void* hilo_funcion_con_mutex(void* arg) {
+  pthread_mutex_lock(&mutex); // Bloquear el mutex 
+  printf("Hola desde el hilo %ld\n", (long) arg); 
+  pthread_mutex_unlock(&mutex); // Desbloquear el mutex 
+  pthread_exit(NULL);
+}
 int main() {
 	pthread_t hilo1, hilo2;
 	// Creación de los hilos
-	pthread_create(&hilo1, NULL, hilo_funcion, (void*)1); 
-	pthread_create(&hilo2, NULL, hilo_funcion, (void*)2);
+	pthread_mutex_init(&mutex,NULL);
+	
+	pthread_create(&hilo1, NULL, hilo_funcion_con_mutex, (void*)1); 
+	pthread_create(&hilo2, NULL, hilo_funcion_con_mutex, (void*)2);
 	// Esperar a que los hilos terminen
 	pthread_join(hilo1, NULL);
 	pthread_join(hilo2, NULL);
+	pthread_mutex_destroy(&mutex);
 	printf("Finalizó la ejecución del programa principal\n");
 	return 0;
 }
